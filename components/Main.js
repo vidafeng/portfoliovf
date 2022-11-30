3b3ac04b1612d52c282750a0762cc67115d47b64
Vida Feng <vidaffeng@gmail.com>
2022-11-30 16:51:15 -0500

    added contact buttons

---
+++ components/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            I build things for the web
+          </p>
+          <h1 className="text-gray-700 py-4">
+            Hi, I'm <span className="text-[#F08E80]">Vida</span>.
+          </h1>
+          <h1 className="text-gray-600 py-1">software engineer</h1>
+
+          <p className="text-gray-600 py-4 max-w-[70%] m-auto">
+            Currently based in New York City.
+          </p>
+
+          <div className="flex item-center justify-between max-width-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
