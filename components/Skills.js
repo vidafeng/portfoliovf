2f336780cd1b0595ac92fbc4c155f848c030287b
Vida Feng <vidaffeng@gmail.com>
2022-12-05 16:35:22 -0500

    flyio deply

---
+++ components/Skills.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "next/image";
+import Css from "../public/assets/skills/css.png";
+import Github from "../public/assets/skills/github.png";
+import Html from "../public/assets/skills/html.png";
+
+const Skills = () => {
+  return (
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#F08E80]">
+          Skills
+        </p>
+        <h2 className="py-4">Technologies I've Worked With</h2>
+        {/* grid container */}
+        {/* auto 1 col on small devices // md 2 cols etc */}
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              {/* images */}
+              <div className="m-auto">
+                <Image src={Html} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              {/* images */}
+              <div className="m-auto">
+                <Image src={Css} width="64px" height="64px" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>CSS</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
